946412bd7787e1d0993c3d69882958bf8c99ae52
Ram <r.ram.wgw@gmail.com>
2013-06-06 15:07:00 +0530

    db config

--- php/getBooks.php
+++ php/getBooks.php
@@ -1,11 +1,11 @@
 ﻿<?php
 $dsn = "pgsql:"
-    . "host=ec2-23-21-130-168.compute-1.amazonaws.com;"
-    . "dbname=d3p8gu2ctu7h4n;"
-    . "user=gictefoytslqtx;"
+    . "host=ec2-23-21-129-229.compute-1.amazonaws.com;"
+    . "dbname=dat4hqf42ko5a9;"
+    . "user=lfltlstiafqcgs;"
     . "port=5432;"
     . "sslmode=require;"
-    . "password=YhDlOuqymvDAh9HPzBZhgO_jH7";
+    . "password=lil1pqUyPo2rkAFptcPTdolpDl";
 $db = new PDO($dsn);
 $query = "SELECT * from book_entity;";
 $result = $db->query($query);
