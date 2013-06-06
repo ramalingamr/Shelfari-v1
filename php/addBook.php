c9c2222412cdc6f8f10a67f138b06c9ce7fbd7ff
Ram <r.ram.wgw@gmail.com>
2013-06-06 14:44:45 +0530

    Trial
    Commit 1

---
+++ php/addBook.php
@@ -0,0 +1,18 @@
+﻿<?php
+$dsn = "pgsql:"
+    . "host=ec2-23-21-130-168.compute-1.amazonaws.com;"
+    . "dbname=d3p8gu2ctu7h4n;"
+    . "user=gictefoytslqtx;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=YhDlOuqymvDAh9HPzBZhgO_jH7";
+$db = new PDO($dsn);
+// Hardcoded user name
+$userName='userX';
+$bookName=$_POST["bookname"];
+$authorName=$_POST["authorname"];
+$status=$_POST["status"];
+$query="INSERT INTO BOOK_ENTITY VALUES('$userName','$bookName','$authorName','$status');";
+$result = $db->query($query);
+echo json_encode($result);
+?>
---
+++ php/getBooks.php
@@ -0,0 +1,15 @@
+﻿<?php
+$dsn = "pgsql:"
+    . "host=ec2-23-21-130-168.compute-1.amazonaws.com;"
+    . "dbname=d3p8gu2ctu7h4n;"
+    . "user=gictefoytslqtx;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=YhDlOuqymvDAh9HPzBZhgO_jH7";
+$db = new PDO($dsn);
+$query = "SELECT * from book_entity;";
+$result = $db->query($query);
+$rows = $result->fetchAll(PDO::FETCH_ASSOC);
+$result->closeCursor();
+echo json_encode($rows);
+?>
